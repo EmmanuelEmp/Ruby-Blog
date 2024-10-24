bd079edaa39c81ab8b04229b0eb2b32a2111d94a
EmmanuelEmp <emmyochogwu@gmail.com>
2024-10-24 06:00:13 +0100

    Feat: User model, factories and migration creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :name, 
+               presence: true,
+               uniqueness: true
+
+    validates :email, 
+               presence: true,
+               uniqueness: true,
+               format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
